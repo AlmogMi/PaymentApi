dc9b88450b78c68ce380029b15b0d9ce268b9c55
Almog Mishraki <mog7599@gmail.com>
2023-11-27 10:23:09 +0200

    existing project

---
+++ PaymentApi/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetailContext :DbContext
+    {
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
